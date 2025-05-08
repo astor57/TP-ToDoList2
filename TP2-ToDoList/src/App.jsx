9229e934c6e49b0edfb7845af16c11225baf90ab
ASTOR FEDERICI <48794543@ort>
2025-05-08 11:35:17 -0300

    Terminado

--- TP2-ToDoList/src/App.jsx
+++ TP2-ToDoList/src/App.jsx
@@ -1,20 +1,11 @@
 import React from "react";
-import Encabezado from "./componentes/Encabezado";
-import Formulario from "./componentes/Formulario";
-import FiltroTareas from "./componentes/FiltroTareas";
-import ListaTareas from "./componentes/ListaTareas";
-import TareaMasRapida from "./componentes/TareaMasRapida";
-import BotonEliminarCompletadas from "./componentes/BotonEliminarCompletadas";
+import Todo from "./componentes/todolist";
+import './App.css'
 
 function App() {
   return (
-    <div className="container mt-5">
-      <Encabezado />
-      <Formulario />
-      <FiltroTareas />
-      <ListaTareas />
-      <TareaMasRapida />
-      <BotonEliminarCompletadas />
+    <div className="todo">
+      <Todo />
     </div>
   );
 }
